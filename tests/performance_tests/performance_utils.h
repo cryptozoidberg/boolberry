207a374b5adfd65449d6b45c1ccd3db6edd59dd0
Harel Altmann <harel.altmann@outlook.com>
2014-04-25 06:54:07 +0400

    performance_tests/performance_utils osx' specific stubs

--- tests/performance_tests/performance_utils.h
+++ tests/performance_tests/performance_utils.h
@@ -6,26 +6,29 @@
 
 #include <iostream>
 
-#include <boost/config.hpp>
-
-#ifdef BOOST_WINDOWS
+#ifdef WIN32
 #include <windows.h>
+#else
+#include <sched.h>
+#include <pthread.h>
 #endif
 
 void set_process_affinity(int core)
 {
-#if defined(BOOST_WINDOWS)
+#if defined(WIN32)
   DWORD_PTR mask = 1;
   for (int i = 0; i < core; ++i)
   {
     mask <<= 1;
   }
-  ::SetProcessAffinityMask(::GetCurrentProcess(), core);
-#elif defined(BOOST_HAS_PTHREADS)
+  SetProcessAffinityMask(GetCurrentProcess(), core);
+#elif defined(__MACH__)
+//#warning set_process_affinity is not currently supported on MacOS
+#else
   cpu_set_t cpuset;
   CPU_ZERO(&cpuset);
   CPU_SET(core, &cpuset);
-  if (0 != ::pthread_setaffinity_np(::pthread_self(), sizeof(cpuset), &cpuset))
+  if (0 != pthread_setaffinity_np(pthread_self(), sizeof(cpuset), &cpuset))
   {
     std::cout << "pthread_setaffinity_np - ERROR" << std::endl;
   }
@@ -34,22 +37,24 @@ void set_process_affinity(int core)
 
 void set_thread_high_priority()
 {
-#if defined(BOOST_WINDOWS)
-  ::SetPriorityClass(::GetCurrentProcess(), HIGH_PRIORITY_CLASS);
-#elif defined(BOOST_HAS_PTHREADS)
+#if defined(WIN32)
+  SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
+#elif defined(__MACH__)
+//#warning set_thread_high_priority is not currently supported on MacOS
+#else
   pthread_attr_t attr;
   int policy = 0;
   int max_prio_for_policy = 0;
 
-  ::pthread_attr_init(&attr);
-  ::pthread_attr_getschedpolicy(&attr, &policy);
-  max_prio_for_policy = ::sched_get_priority_max(policy);
+  pthread_attr_init(&attr);
+  pthread_attr_getschedpolicy(&attr, &policy);
+  max_prio_for_policy = sched_get_priority_max(policy);
 
-  if (0 != ::pthread_setschedprio(::pthread_self(), max_prio_for_policy))
+  if (0 != pthread_setschedprio(pthread_self(), max_prio_for_policy))
   {
     std::cout << "pthread_setschedprio - ERROR" << std::endl;
   }
 
-  ::pthread_attr_destroy(&attr);
+  pthread_attr_destroy(&attr);
 #endif
 }
