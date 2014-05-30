f7e266102bbd550213241d43cf3a1fdc2c9cf395
cr.zoidberg <crypto.zoidberg@gmail.com>
2014-05-30 03:37:16 +0300

    broken build fix

--- tests/functional_tests/transactions_flow_test.cpp
+++ tests/functional_tests/transactions_flow_test.cpp
@@ -52,7 +52,7 @@ bool do_send_money(tools::wallet2& w1, tools::wallet2& w2, size_t mix_in_factor,
 
   try
   {
-    w1.transfer(dsts, mix_in_factor, 0, DEFAULT_FEE, tools::detail::null_split_strategy, tools::tx_dust_policy(DEFAULT_FEE), tx);
+    w1.transfer(dsts, mix_in_factor, 0, DEFAULT_FEE, std::vector<uint8_t>(), tools::detail::null_split_strategy, tools::tx_dust_policy(DEFAULT_FEE), tx);
     return true;
   }
   catch (const std::exception&)
