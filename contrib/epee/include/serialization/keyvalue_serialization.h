53da249a2e6533e70901fddcb427e6d96230b920
cr.zoidberg <crypto.zoidberg@gmail.com>
2014-06-27 22:23:56 +0300

    fixed serialization bug

--- contrib/epee/include/serialization/keyvalue_serialization.h
+++ contrib/epee/include/serialization/keyvalue_serialization.h
@@ -86,7 +86,7 @@ public: \
 #define KV_SERIALIZE_CONTAINER_POD_AS_BLOB(varialble)     KV_SERIALIZE_CONTAINER_POD_AS_BLOB_N(varialble, #varialble)
 
 #define KV_CHAIN_MAP(variable_obj) epee::namespace_accessor<decltype(this_ref.variable_obj)>::template serialize_map<is_store>(this_ref.variable_obj, stg, hparent_section);
-#define KV_CHAIN_BASE(base_type) base_type::serialize_map<is_store>(static_cast<base_type>(this_ref), stg, hparent_section);
+#define KV_CHAIN_BASE(base_type) base_type::serialize_map<is_store>(static_cast<base_type&>(this_ref), stg, hparent_section);
 }
 
 
