ff22e44c8aad39eb8c5398ddb8673e577ef73901
longlegluvak <longlegluvak@gmail.com>
2015-04-19 01:58:46 +0800

    reset blockchain

--- src/currency_core/blockchain_storage.h
+++ src/currency_core/blockchain_storage.h
@@ -322,7 +322,7 @@ namespace currency
   /*                                                                      */
   /************************************************************************/
 
-  #define CURRENT_BLOCKCHAIN_STORAGE_ARCHIVE_VER          33
+  #define CURRENT_BLOCKCHAIN_STORAGE_ARCHIVE_VER          34
   #define CURRENT_TRANSACTION_CHAIN_ENTRY_ARCHIVE_VER     3
   #define CURRENT_BLOCK_EXTENDED_INFO_ARCHIVE_VER         1
 
@@ -357,7 +357,7 @@ namespace currency
   template<class archive_t>
   void blockchain_storage::serialize(archive_t & ar, const unsigned int version)
   {
-    if(version < 33)
+    if(version < 34)
       return;
     CHECK_PROJECT_NAME();
     CRITICAL_REGION_LOCAL(m_blockchain_lock);
