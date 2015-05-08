7c1bcfd7554583196b770ccf5f97eb0a505b730a
longlegluvak <longlegluvak@gmail.com>
2015-05-08 23:50:19 +0800

    added reset wallet api2

--- src/gui/qt-daemon/html5applicationviewer/html5applicationviewer.cpp
+++ src/gui/qt-daemon/html5applicationviewer/html5applicationviewer.cpp
@@ -803,7 +803,7 @@ QString Html5ApplicationViewer::resync_wallet(const QString& param)
   return que_call2<view::wallet_id_obj>("get_wallet_info", param, [this](const view::wallet_id_obj& a, view::api_response& ar){
 
     view::wallet_info wi = AUTO_VAL_INIT(wi);
-    ar.error_code = m_backend.get_wallet_info(a.wallet_id, wi);
+    ar.error_code = m_backend.resync_wallet(a.wallet_id);
     dispatch(ar, wi);
   });
 }
