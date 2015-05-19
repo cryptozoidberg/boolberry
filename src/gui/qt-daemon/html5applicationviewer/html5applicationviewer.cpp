da9bd86ffae5fac6aef4f5cfaf5e34d5cfa065b6
longlegluvak <longlegluvak@gmail.com>
2015-05-19 23:14:39 +0800

    broken build fix

--- src/gui/qt-daemon/html5applicationviewer/html5applicationviewer.cpp
+++ src/gui/qt-daemon/html5applicationviewer/html5applicationviewer.cpp
@@ -821,7 +821,7 @@ QString Html5ApplicationViewer::run_wallet(const QString& param)
 
 }
 
-QString Html5ApplicationViewer::get_wallet_info(const QString& param)
+/*QString Html5ApplicationViewer::get_wallet_info(const QString& param)
 {
   return que_call2<view::wallet_id_obj>("get_wallet_info", param, [this](const view::wallet_id_obj& a, view::api_response& ar){
 
@@ -829,7 +829,7 @@ QString Html5ApplicationViewer::get_wallet_info(const QString& param)
     ar.error_code = m_backend.get_wallet_info(a.wallet_id, wi);
     dispatch(ar, wi);
   });
-}
+}*/
 
 QString Html5ApplicationViewer::resync_wallet(const QString& param)
 {
