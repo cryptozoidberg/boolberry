32cde362a1240c9516862f24e64c325ff7599012
longlegluvak <longlegluvak@gmail.com>
2015-06-23 13:01:00 +0800

    fixed broken build

--- src/currency_core/account.cpp
+++ src/currency_core/account.cpp
@@ -45,7 +45,7 @@ DISABLE_VS_WARNINGS(4244 4345)
   //-----------------------------------------------------------------
   std::string account_base::get_restore_data() const
   {
-    return std::string(&m_keys.m_spend_secret_key.data[0], sizeof(m_keys.m_spend_secret_key.data));
+    return std::string(&m_keys.m_spend_secret_key, sizeof(m_keys.m_spend_secret_key));
   }
   //-----------------------------------------------------------------
   bool account_base::restore_keys(const std::string& restore_data)
