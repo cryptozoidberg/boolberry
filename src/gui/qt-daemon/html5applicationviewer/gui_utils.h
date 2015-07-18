8762c291bec17afef6127660102112880eb8c136
longlegluvak <longlegluvak@gmail.com>
2015-07-18 20:02:54 +0800

    autostart implemented

---
+++ src/gui/qt-daemon/html5applicationviewer/gui_utils.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2011-2013 The Bitcoin Core developers
+// Copyright (c) 2015 Boolberry developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace
+{
+  bool GetStartOnSystemStartup();
+  bool SetStartOnSystemStartup(bool fAutoStart);
+}
