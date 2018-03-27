372cb5a4669202341d7235fa806b7044d055b7ca
wnntaler <wnntaler@boolberry.com>
2018-03-27 05:47:29 +0300

    lmdb unittest was improved a little

--- tests/unit_tests/lmdb_test.cpp
+++ tests/unit_tests/lmdb_test.cpp
@@ -9,15 +9,50 @@ namespace
 {
   TEST(lmdb, test1)
   {
-    db::lmdb_adapter lmdb;
-    db::db_bridge_base dbb(&lmdb);
+    std::shared_ptr<db::lmdb_adapter> lmdb_ptr = std::make_shared<db::lmdb_adapter>();
+    db::db_bridge_base dbb(lmdb_ptr);
 
     bool r = false;
     
     r = dbb.open("test_lmdb");
     ASSERT_TRUE(r);
 
+    db::table_id tid_decapod;
+    r = lmdb_ptr->open_table("decapod", tid_decapod);
+    ASSERT_TRUE(r);
+
+    ASSERT_TRUE(lmdb_ptr->begin_transaction());
+
+    ASSERT_TRUE(lmdb_ptr->clear_table(tid_decapod));
+
+    uint64_t key = 10;
+    std::string buf = "nxjdu47flrp20soam19e7nfhxbcy48owks03of92sbf31n1oqkanmdb47";
+
+    r = lmdb_ptr->set(tid_decapod, (char*)&key, sizeof key, buf.c_str(), buf.size());
+    ASSERT_TRUE(r);
+
+    ASSERT_TRUE(lmdb_ptr->commit_transaction());
+
     r = dbb.close();
     ASSERT_TRUE(r);
+
+
+    r = dbb.open("test_lmdb");
+    ASSERT_TRUE(r);
+    r = lmdb_ptr->open_table("decapod", tid_decapod);
+    ASSERT_TRUE(r);
+
+    ASSERT_TRUE(lmdb_ptr->begin_transaction());
+
+    std::string out_buffer;
+    r = lmdb_ptr->get(tid_decapod, (char*)&key, sizeof key, out_buffer);
+    ASSERT_TRUE(r);
+    ASSERT_EQ(buf, out_buffer);
+
+    ASSERT_TRUE(lmdb_ptr->commit_transaction());
+
+    r = dbb.close();
+    ASSERT_TRUE(r);
+
   }
 }
