c38360cba65f85a91f02195c968fd4e5cd59f161
wnntaler <wnntaler@boolberry.com>
2018-03-30 06:56:06 +0300

    added PRNG state manipulation routines (potentially insecure, for tests and debug only)

--- tests/unit_tests/lmdb_test.cpp
+++ tests/unit_tests/lmdb_test.cpp
@@ -1,6 +1,11 @@
 // Copyright (c) 2012-2018 The Cryptonote developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+extern "C"
+{
+  #define USE_INSECURE_RANDOM_RPNG_ROUTINES
+  #include "crypto/random.h"
+}
 #include "gtest/gtest.h"
 #include "common/db_bridge.h"
 #include "common/db_lmdb_adapter.h"
