89c521a8514c099a3a754a97c1c8719e27a2e437
cr.zoidberg <crypto.zoidberg@gmail.com>
2018-05-24 14:39:01 +0300

    attempt to fix duplicate iterator error in pool

--- src/currency_core/tx_pool.cpp
+++ src/currency_core/tx_pool.cpp
@@ -209,6 +209,7 @@ namespace currency
          (tx_age > CURRENCY_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME && it->second.kept_by_block) )
       {
         LOG_PRINT_L0("Tx " << it->first << " removed from tx pool due to outdated, age: " << tx_age );
+        remove_transaction_keyimages(it->second.tx);
         m_transactions.erase(it++);
       }else
         ++it;
