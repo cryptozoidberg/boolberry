a3ce0315bdb7bd0e09ce7d7ce290d6425d553991
cr.zoidberg <crypto.zoidberg@gmail.com>
2018-08-31 17:20:43 +0300

    fixed initialization bug2

--- src/currency_core/blockchain_storage.cpp
+++ src/currency_core/blockchain_storage.cpp
@@ -137,11 +137,11 @@ bool blockchain_storage::init(const boost::program_options::variables_map& vm, c
   bool res = m_lmdb_adapter->init(vm);
   CHECK_AND_ASSERT_MES(res, false, "Unable to init lmdb adapter");
 
+  m_config_folder = config_folder;
   LOG_PRINT_L0("Loading blockchain...");
   const std::string folder_name = m_config_folder + "/" CURRENCY_BLOCKCHAINDATA_FOLDERNAME;
   tools::create_directories_if_necessary(folder_name);
 
-  m_config_folder = config_folder;
   if (!check_instance(m_config_folder))
     return false;
 
