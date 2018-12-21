9de6bd750e93173de050db853c1f932c7c8a1e46
cr.zoidberg <crypto.zoidberg@gmail.com>
2018-12-21 19:13:17 +0300

    changed predownload progress layout

--- src/common/pre_download.h
+++ src/common/pre_download.h
@@ -22,7 +22,8 @@ namespace tools
     uint64_t unpacked_size;
   };
 #ifndef TESTNET
-  const static pre_download_entrie pre_download = { "http://88.99.193.104/downloads/data3.mdb.pak", "852ad69914faf6c2e5220b719b30bb4a", 5769499320, 7884759040 };
+//  const static pre_download_entrie pre_download = { "http://88.99.193.104/downloads/data3.mdb.pak", "852ad69914faf6c2e5220b719b30bb4a", 5769499320, 7884759040 };
+  const static pre_download_entrie pre_download = { "http://x8s8i7z7.stackpathcdn.com/downloads/data3.mdb.pak", "852ad69914faf6c2e5220b719b30bb4a", 5769499320, 7884759040 };
 #else
   const static pre_download_entrie pre_download = { "http://88.99.193.104/downloads/data_testnet.mdb.pak", "57feaa97401048386f335355d23fdf18", 164782602, 238563328 };
 #endif
@@ -61,7 +62,7 @@ namespace tools
       }
 
 
-      std::cout << "Received " << received_bytes << " from " << total_bytes << " (" << (received_bytes * 100) / total_bytes << "%)\r";
+      std::cout << "Received " << received_bytes/1048576 << "Mb from " << total_bytes/1048576 << "Mb (" << (received_bytes * 100) / total_bytes << "%)\r";
       if (is_stop(total_bytes, received_bytes))
       {
         LOG_PRINT_MAGENTA(ENDL << "Interrupting download", LOG_LEVEL_0);
